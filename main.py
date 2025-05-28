e0b3ed8e54c7ec44af7acbe65266773309f48eab
millenagualandi <millenagualandi@gmail.com>
2025-05-28 17:46:23 -0300

    Add product registration functionality and enhance product display

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Form
+from fastapi.responses import RedirectResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 import uvicorn
@@ -6,6 +7,7 @@ import uvicorn
 from data import produto_repo
 from data import cliente_repo
 from data import forma_pagamento_repo
+from data.produto_model import Produto
 
 
 app = FastAPI()
@@ -23,29 +25,42 @@ async def get_root():
     response = templates.TemplateResponse("index.html", {"request": {}, "produtos": produtos})
     return response
 
-
 @app.get("/produtos")
 async def get_produtos():
     produtos = produto_repo.obter_todos()
     response = templates.TemplateResponse("produtos.html", {"request": {}, "produtos": produtos})
     return response
 
-
 @app.get("/produtos/{id}")
 async def get_produto_por_id(id: int):
     produto = produto_repo.obter_por_id(id)
     response = templates.TemplateResponse("produto.html", {"request": {}, "produto": produto})
     return response
-    
 
+@app.get("/admin/produtos/cadastrar")
+async def get_produto_cadastrar():
+    response = templates.TemplateResponse("cadastrar_produto.html", {"request": {}})
+    return response
 
+@app.post("/admin/produtos/cadastrar")
+async def post_produto_cadastrar(
+    nome: str = Form(...), 
+    descricao: str = Form(...), 
+    preco: float = Form(...), 
+    quantidade: int = Form(...)
+):
+    produto = Produto(0, nome, descricao, preco, quantidade)
+    id_produto = produto_repo.inserir(produto)
+    if id_produto == None:
+        raise Exception("Erro ao inserir produto.")
+    else:
+        return RedirectResponse("/produtos", status_code=303)
 
 @app.get("/clientes")
 async def get_clientes():
     clientes = cliente_repo.obter_todos()
     response = templates.TemplateResponse("clientes.html", {"request": {}, "clientes": clientes})
     return response
-
 
 @app.get("/formas_pagamento")
 async def get_formas_pagamento():
